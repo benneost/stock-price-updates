05a1ba156393f2f4e7f5b9e3c76b9ed1969bba20
Benjamin Neo <benneost@gmail.com>
2022-07-29 20:13:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -51,10 +51,10 @@ def main() -> None:
     # Start the Bot
     # `start_polling` for local dev; webhook for production
     # updater.start_polling()
-    updater.start_webhook(
-        listen="0.0.0.0",
-        port=int(PORT),
-        url_path=TOKEN
+    updater.start_webhook(listen="0.0.0.0",
+        port=int(os.environ.get('PORT', 5000)),
+        url_path=TOKEN,
+        webhook_url="https://api.telegram.org/bot" + TOKEN + "/setWebhook?url=https://stockbot-telegram.herokuapp.com"
     )
     updater.bot.setWebhook("https://api.telegram.org/bot" + TOKEN + "/setWebhook?url=https://stockbot-telegram.herokuapp.com")
     
